94397b0f7bfc116012d89dd98f7776fd6def67a5
scd7896 <scd1212@naver.com>
2020-12-01 00:27:52 +0900

    Fix: 기초 셋팅

---
+++ pages/api/user.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "controller";
+
+const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+	if (_req.method === "GET") {
+		try {
+			// const userNum = _req.headers.authorization;
+			// if (!userNum) {
+			// 	return res.status(401).json({ status: "fail", message: "유저 정보가 없습니다" });
+			// }
+			// const user = await User.findByEmail(1);
+
+			res.status(200).json({ status: "success", payload: { hello: "world" } });
+		} catch (err) {
+			res.status(500).json({ status: "error", message: "데이터베이스 오류" });
+		}
+	}
+
+	if (_req.method === "POST") {
+		try {
+			const user = await User.userPost({ ..._req.body });
+			res.status(200).json({ status: "success", payload: user });
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({ status: "error", message: "데이터베이스 오류" });
+		}
+	}
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import axios from "axios";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+	const userAddTest = () => {
+		axios.post("/api/user", { email: "test1@test.com", name: "test" });
+	};
+	return (
+		<Layout title="Home | Next.js + TypeScript Example">
+			<h1>Hello Next.js 👋</h1>
+			<p onClick={userAddTest}>눌러라눌러</p>
+		</Layout>
+	);
+};
 
-export default IndexPage
+export default IndexPage;
